8815674eab558fddc08a32df42d84a5d83053a90
Christiaan <c.molendijk@chrismo.nl>
2021-08-20 16:57:20 +0200

    Remove top bar and added account button

--- app/core/components/layout/nav.tsx
+++ app/core/components/layout/nav.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Flex, Button, IconButton, Box, Text, Link, Image } from "@chakra-ui/react"
+import { Flex, Button, IconButton, Box, Text, Divider } from "@chakra-ui/react"
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons"
 import NextLink from "next/link"
 import { useTranslations } from "next-intl"
@@ -7,71 +7,11 @@ import { useRouter } from "blitz"
 
 export const Nav = () => {
   const router = useRouter()
-  const { locale } = router
   const t = useTranslations("Nav")
   const [display, changeDisplay] = useState("none")
 
-  let languageLink
-  if (locale === "en") {
-    languageLink = (
-      <NextLink href="/" locale="nl">
-        <Image
-          src="/images/icons/nl.png"
-          alt={t("dutch-option-alt") as string}
-          title={t("dutch-option-title") as string}
-          width="24px"
-          height="24px"
-          my="3px"
-          mx={2}
-        />
-      </NextLink>
-    )
-  } else {
-    languageLink = (
-      <NextLink href="/" locale="en">
-        <Image
-          src="/images/icons/en.png"
-          alt={t("english-option-alt") as string}
-          title={t("english-option-title") as string}
-          width="24px"
-          height="24px"
-          my="3px"
-          mx={2}
-        />
-      </NextLink>
-    )
-  }
-
   return (
     <>
-      <Box
-        backgroundColor="white"
-        color="blue.500"
-        height={30}
-        display={["none", "none", "flex", "flex"]}
-      >
-        <Flex position="fixed" right="1rem" align="right">
-          <Link
-            to="/account/login"
-            my={1}
-            mx={2}
-            title={t("login-link-title") as string}
-            fontSize="sm"
-          >
-            {t("login-link-label")}
-          </Link>
-          <Link
-            to="/account/login"
-            my={1}
-            mx={2}
-            title={t("register-link-title") as string}
-            fontSize="sm"
-          >
-            {t("register-link-label")}
-          </Link>
-          {languageLink}
-        </Flex>
-      </Box>
       <Flex backgroundColor="blue.500" color="white">
         <Box m={3}>
           <Text fontSize="1xl" display="inline-block" fontWeight="bold">
@@ -90,11 +30,11 @@ export const Nav = () => {
             <NextLink href="/buy" passHref>
               <Button
                 as="a"
-                variant="ghost"
-                aria-label="Home"
+                variant="link"
+                aria-label={t("buy-link-label") as string}
                 my={2}
-                w="100%"
-                color="white"
+                mx={2}
+                color="link"
                 _hover={{
                   color: "brand.500",
                 }}
@@ -107,10 +47,10 @@ export const Nav = () => {
             <NextLink href="/sell" passHref>
               <Button
                 as="a"
-                variant="ghost"
+                variant="link"
                 aria-label={t("sell-link-label") as string}
                 my={2}
-                w="100%"
+                mx={2}
                 color="white"
                 _hover={{
                   color: "brand.500",
@@ -123,10 +63,10 @@ export const Nav = () => {
             <NextLink href="/learn" passHref>
               <Button
                 as="a"
-                variant="ghost"
+                variant="link"
                 aria-label={t("learn-link-label") as string}
                 my={2}
-                w="100%"
+                mx={2}
                 color="white"
                 _hover={{
                   color: "brand.500",
@@ -139,10 +79,10 @@ export const Nav = () => {
             <NextLink href="/contact" passHref>
               <Button
                 as="a"
-                variant="ghost"
+                variant="link"
                 aria-label={t("contact-link-label") as string}
                 my={2}
-                w="100%"
+                mx={2}
                 color="white"
                 _hover={{
                   color: "brand.500",
@@ -152,17 +92,37 @@ export const Nav = () => {
                 {t("contact-link-label")}
               </Button>
             </NextLink>
+            <NextLink href="/account" passHref>
+              <Button
+                as="a"
+                variant="outline"
+                aria-label={t("account-link-label") as string}
+                my={2}
+                mx={2}
+                color="white"
+                _hover={{
+                  color: "brand.500",
+                }}
+                title={t("account-link-title") as string}
+              >
+                {t("account-link-label")}
+              </Button>
+            </NextLink>
           </Flex>
 
           {/* Mobile */}
-          <IconButton
-            aria-label="Open Menu"
-            size="lg"
-            mr={2}
-            icon={<HamburgerIcon />}
-            onClick={() => changeDisplay("flex")}
-            display={["flex", "flex", "none", "none"]}
-          />
+
+          <Flex justify="flex-end" display={["flex", "flex", "none", "none"]}>
+            <IconButton
+              mt={2}
+              mr={0}
+              aria-label="Open Menu"
+              size="md"
+              icon={<HamburgerIcon />}
+              onClick={() => changeDisplay("flex")}
+              backgroundColor="transparent"
+            />
+          </Flex>
         </Flex>
 
         {/* Mobile Content */}
@@ -182,35 +142,97 @@ export const Nav = () => {
             <IconButton
               mt={2}
               mr={2}
-              aria-label="Open Menu"
-              size="lg"
+              aria-label="Close Menu"
+              size="md"
               icon={<CloseIcon />}
               onClick={() => changeDisplay("none")}
+              backgroundColor="transparent"
             />
           </Flex>
 
           <Flex flexDir="column" align="center">
-            <NextLink href="/" passHref>
-              <Button as="a" variant="ghost" aria-label="Home" my={5} w="100%">
-                Home
+            <NextLink href="/buy" passHref>
+              <Button
+                as="a"
+                variant="link"
+                aria-label="Home"
+                my={2}
+                w="100%"
+                color="white"
+                _hover={{
+                  color: "blue.500",
+                }}
+                title={t("buy-link-title") as string}
+              >
+                {t("buy-link-label")}
               </Button>
             </NextLink>
 
-            <NextLink href="/about" passHref>
-              <Button as="a" variant="ghost" aria-label="About" my={5} w="100%">
-                About
+            <NextLink href="/sell" passHref>
+              <Button
+                as="a"
+                variant="link"
+                aria-label={t("sell-link-label") as string}
+                my={2}
+                w="100%"
+                color="white"
+                _hover={{
+                  color: "blue.500",
+                }}
+                title={t("sell-link-title") as string}
+              >
+                {t("sell-link-label")}
               </Button>
             </NextLink>
-
+            <NextLink href="/learn" passHref>
+              <Button
+                as="a"
+                variant="link"
+                aria-label={t("learn-link-label") as string}
+                my={2}
+                w="100%"
+                color="white"
+                _hover={{
+                  color: "blue.500",
+                }}
+                title={t("learn-link-title") as string}
+              >
+                {t("learn-link-label")}
+              </Button>
+            </NextLink>
             <NextLink href="/contact" passHref>
-              <Button as="a" variant="ghost" aria-label="Contact" my={5} w="100%">
-                Contact
+              <Button
+                as="a"
+                variant="link"
+                aria-label={t("contact-link-label") as string}
+                my={2}
+                w="100%"
+                color="white"
+                _hover={{
+                  color: "blue.500",
+                }}
+                title={t("contact-link-title") as string}
+              >
+                {t("contact-link-label")}
               </Button>
             </NextLink>
-
-            <NextLink href="/account/login">{t("login-link-label")}</NextLink>
-            <NextLink href="/account/login">{t("register-link-label")}</NextLink>
-            {languageLink}
+            <Divider my={2} />
+            <NextLink href="/account" passHref>
+              <Button
+                as="a"
+                variant="link"
+                aria-label={t("account-link-label") as string}
+                my={2}
+                w="100%"
+                color="white"
+                _hover={{
+                  color: "blue.500",
+                }}
+                title={t("account-link-title") as string}
+              >
+                {t("account-link-label")}
+              </Button>
+            </NextLink>
           </Flex>
         </Flex>
       </Flex>
