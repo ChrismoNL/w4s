fb79117eea54988939dc0a6d963ac4de266b7046
Christiaan <c.molendijk@chrismo.nl>
2021-08-23 07:44:05 +0200

    Improve footer for mobile

--- app/core/components/layout/footer.tsx
+++ app/core/components/layout/footer.tsx
@@ -17,8 +17,8 @@ export const Footer = () => {
   }
 
   return (
-    <Box as="footer" p={5} backgroundColor="blue.500" color="white" fontSize="sm">
-      <Flex>
+    <Box as="footer" p={5} backgroundColor="blue.500" color="white" fontSize="sm" w="100vw">
+      <Flex display={["none", "none", "flex", "flex"]}>
         <Flex flex={1} pt={2}>
           <Stack direction="row">
             <Text fontWeight="bold">&copy; Webshop 4 Sale</Text>
@@ -41,7 +41,7 @@ export const Footer = () => {
                 {t("privacy-link-label")}
               </Button>
             </NextLink>
-            <Text>-</Text>
+            <Text>&bull;</Text>
             <NextLink href="/terms" passHref>
               <Button
                 as="a"
@@ -61,7 +61,7 @@ export const Footer = () => {
                 {t("terms-link-label")}
               </Button>
             </NextLink>
-            <Text>-</Text>
+            <Text>&bull;</Text>
             <NextLink href="/sitemap" passHref>
               <Button
                 as="a"
@@ -99,6 +99,83 @@ export const Footer = () => {
           </Select>
         </Stack>
       </Flex>
+      <Stack
+        display={["flex", "flex", "none", "none"]}
+        align="center"
+        alignContent="center"
+        zIndex={0}
+      >
+        <Text fontWeight="bold">&copy; Webshop 4 Sale</Text>
+        <NextLink href="/privacy" passHref>
+          <Button
+            as="a"
+            variant="link"
+            aria-label={t("privacy-link-label") as string}
+            pt={8}
+            pb={2}
+            height="20px"
+            fontWeight="normal"
+            color="white"
+            fontSize="sm"
+            _hover={{
+              color: "brand.500",
+            }}
+            title={t("privacy-link-title") as string}
+          >
+            {t("privacy-link-label")}
+          </Button>
+        </NextLink>
+        <NextLink href="/terms" passHref>
+          <Button
+            as="a"
+            variant="link"
+            aria-label={t("terms-link-label") as string}
+            pt={2}
+            pb={2}
+            height="20px"
+            fontWeight="normal"
+            color="white"
+            fontSize="sm"
+            _hover={{
+              color: "brand.500",
+            }}
+            title={t("terms-link-title") as string}
+          >
+            {t("terms-link-label")}
+          </Button>
+        </NextLink>
+        <NextLink href="/sitemap" passHref>
+          <Button
+            as="a"
+            variant="link"
+            aria-label={t("sitemap-link-label") as string}
+            pt={2}
+            pb={2}
+            height="20px"
+            fontWeight="normal"
+            color="white"
+            fontSize="sm"
+            _hover={{
+              color: "brand.500",
+            }}
+            title={t("sitemap-link-title") as string}
+          >
+            {t("sitemap-link-label")}
+          </Button>
+        </NextLink>
+        <Text pt={4}>{t("language")}:</Text>
+        <Select
+          variant="flushed"
+          fontSize="sm"
+          size="sm"
+          w="50vw"
+          value={locale}
+          onChange={handleLocaleChange}
+        >
+          <option value="en">English</option>
+          <option value="nl">Nederlands</option>
+        </Select>
+      </Stack>
     </Box>
   )
 }
